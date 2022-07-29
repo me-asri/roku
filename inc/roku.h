89f178d85903e9d2c623f632dcf5c5954707156f
Mehrzad Asri <mehrzad.asri@protonmail.ch>
2022-07-30 01:36:31 +0430

    Adhere to RFC 7335

--- inc/roku.h
+++ inc/roku.h
@@ -9,11 +9,14 @@
 #define IF_MAX_MTU 65535
 #define BUF_SIZE 65535
 
-#define DEFAULT_MTU 1500
-#define DEFAULT_GW "10.6.4.1"
-#define DEFAULT_IP "10.6.4.2"
+// IP addresses from the range defined in RFC 7335
+#define DEFAULT_GW "192.0.0.1"
+#define DEFAULT_IP "192.0.0.2"
+
 #define DEFAULT_IP6_PREFIX "fd64:64:64::"
+
 #define DEFAULT_IFNAME "roku"
+#define DEFAULT_MTU 1500
 
 #define ROUTE_METRIC 2000
 #define ROUTE_MTU (roku_cfg.mtu - 20) // MTU - MTU_DIFF
